8c44235852c316a158153444a3e3cf4d57233dcb
Hasin Swalah VP <hasiinswalah@protonmail.com>
2026-03-01 12:55:53 +0400

    Revert "Add GitHub Pages deployment workflow"
    This reverts commit 17c2facb8f5cb233dc6d551edaac6a06da7e4392.

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-const repoName = "hasin-portfolio-web";
-
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
-  base: `/${repoName}/`,
   server: {
     host: "::",
     port: 8080,
